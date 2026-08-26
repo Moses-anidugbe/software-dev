1ab2be2aeeae81a2c22857da109b137e451ffe76
Moses Anidugbe <mosesanidugbe@gmail.com>
2026-08-26 15:55:16 +0100

    fixed seller post route

--- digital-asset-store-api/routes/products.js
+++ digital-asset-store-api/routes/products.js
@@ -10,21 +10,23 @@ router.post("/", authenticateToken, requireRole("seller"), async (req, res) => {
   // Process the request (e.g., save to database)
   try {
     const result = await client.query(
-      `INSERT INTO products (title, price, download_url) VALUES ($1, $2, $3) RETURNING *`,
-      [title, price, download_url],
+      `INSERT INTO products (title, price, download_url, seller_id) VALUES ($1, $2, $3, $4) RETURNING *`,
+      [title, price, download_url, req.user.id],
     );
 
     const createdProduct = result.rows[0];
+
+    res.status(201).json({
+      message: "Product created successfully.",
+      title: createdProduct.title,
+      price: createdProduct.price,
+      download_url: createdProduct.download_url,
+      seller_id: createdProduct.seller_id,
+    });
   } catch (error) {
     console.error("Error occurred while creating product:", error);
     return res.status(500).json({ message: "Internal server error." });
   }
-  res.status(201).json({
-    message: "Product created successfully.",
-    title: createdProduct.title,
-    price: createdProduct.price,
-    download_url: createdProduct.download_url,
-  });
 });
 
 router.get("/", async (req, res) => {
@@ -37,22 +39,27 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/:id/buy", async (req, res) => {
-  const { id } = req.params;
-  // Process the request (e.g., update product inventory)
-  try {
-    const result = await client.query(
-      "UPDATE products SET inventory = inventory - 1 WHERE id = $1 RETURNING *",
-      [id],
-    );
-    if (result.rows.length === 0) {
-      return res.status(404).json({ message: "Product not found." });
+router.post(
+  "/:id/buy",
+  authenticateToken,
+  requireRole("buyer"),
+  async (req, res) => {
+    const { id } = req.params;
+    // Process the request
+    try {
+      const result = await client.query(
+        "UPDATE products SET inventory = inventory - 1 WHERE id = $1 RETURNING *",
+        [id],
+      );
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: "Product not found." });
+      }
+      res.json({ message: `Product with ID ${id} purchased successfully.` });
+    } catch (error) {
+      console.error("Error occurred while purchasing product:", error);
+      return res.status(500).json({ message: "Internal server error." });
     }
-    res.json({ message: `Product with ID ${id} purchased successfully.` });
-  } catch (error) {
-    console.error("Error occurred while purchasing product:", error);
-    return res.status(500).json({ message: "Internal server error." });
-  }
-});
+  },
+);
 
 module.exports = router;
